ed5089bf921ea63b3b7a2a1e667db0e261eb246a
Inko03 <99274731+Inko03@users.noreply.github.com>
2023-06-13 04:14:28 +0200

    Update Shop.jsx

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -26,7 +26,7 @@ export default function Navigation() {
     //Fetch data from api
         useEffect(()=>{
             const getPost = async()=>{
-                await fetch('https://sklep-api.onrender.com/shop')
+                await fetch('https://sklep-api.onrender.com/api/shop')
                 .then(res=>res.json())
                 .then((data)=>{
                     setDane(data)
